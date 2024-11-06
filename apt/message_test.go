562a86bf0e6b5e84a81232bd8c971af1b5131f4c
Eric Norris <enorris@etsy.com>
2024-11-06 14:03:17 -0500

    fix: `apt update` stuck when fields contain trailing \n
    Before this commit, there were a few scenarios where this apt transport
    would format a message containing a field that had a trailing newline,
    which resulted in a message looking like:
    
    ```
    400 URI Failure
    Message: Get "https://us-central1-apt.pkg.dev/projects/<redacted>/dists/<redacted>/InRelease": oauth2/google: status code 503: {
      "error": {
      "code": 503,
      "message": "Authentication backend unavailable.",
      "status": "UNAVAILABLE"
      }
    }
    
    URI: ar+https://us-central1-apt.pkg.dev/projects/<redacted>/dists/<redacted>/InRelease'
    ```
    
    As the above indicates, this can happen when this apt transport
    encounters an error when doing the underlying oauth2 token exchange; it
    directly puts the error message into the response on L199:
    
    https://github.com/GoogleCloudPlatform/artifact-registry-apt-transport/blob/main/apt/method.go#L190-L201
    
    Unfortunately, this causes apt update to treat this response as two
    separate messages, and since the first message does not contain a URI,
    it also doesn't realize that the InRelease fetch failed. This causes apt
    to hang indefinitely.
    
    Here are a snippet of some logs that demonstrate this:
    
    ```
    1712435831 2494987  <- ar+https:400%20URI%20Failure%0aMessage:%20Get%20"https://us-central1-apt.pkg.dev/projects/<redacted>/dists/<redacted>/InRelease":%20oauth2/google:%20status%20code%20503:%20{%0a%20%20"error":%20{%0a%20%20%20%20"code":%20503,%0a%20%20%20%20"message":%20"Authentication%20backend%20unavailable.",%0a%20%20%20%20"status":%20"UNAVAILABLE"%0a%20%20}%0a}
    1712435831 2494987  <- ar+https:URI:%20ar+https://us-central1-apt.pkg.dev/projects/<redacted>/dists/etsy-ubuntu-jammy/InRelease
    1712435831 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    1712435831 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    1712435832 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    1712435832 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    1712435833 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    1712435833 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    1712435834 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    1712435834 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    1712435835 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    1712435835 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    1712435836 2494987 [0.000] Bytes: 0  / 1  # Files: 24 / 28
    ...
    ```
    
    In the above log, each `<-` is a message received by apt. It thinks it
    received two separate messages, cannot match them to any download, and
    waits forever.
    
    After this commit, the message struct encodes newlines as literal "\n"
    characters, so it prevents both trailing newlines and any accidental
    double newlines in the response.

--- apt/message_test.go
+++ apt/message_test.go
@@ -107,6 +107,17 @@ func TestAptWriterWriteMessage(t *testing.T) {
 			},
 			"123 Fake\n\n",
 		},
+		{
+			Message{
+				code:        123,
+				description: "Fake",
+				fields: map[string][]string{
+					"akey": {"val ending with newline\n"},
+					"zkey": {"val containing \n\n double newlines"},
+				},
+			},
+			"123 Fake\nakey: val ending with newline\\n\nzkey: val containing \\n\\n double newlines\n\n",
+		},
 	}
 
 	for _, tt := range tests {
